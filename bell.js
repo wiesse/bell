5fe73e4ef7d2b3ac8a1cb62b588302568af55831
wiesse <34985649+wiesse@users.noreply.github.com>
2022-09-20 08:25:14 -0400

    Add files via upload

--- bell.js
+++ bell.js
@@ -1,41 +1,40 @@
 const a_day = " 9-2 9-4 9-9 9-11 9-15 9-17 9-21 9-23 9-25 9-30 10-2 10-6 10-8 10-12 10-14 10-16 10-20 10-22 10-26 10-28 10-30 11-4 11-6 11-10 11-13 11-17 11-19 11-23 11-25 12-1 12-3 12-7 12-9 12-11 12-15 12-17 12-21 12-23 1-5 1-7 1-11 1-13 1-15 1-20 1-22 1-26 1-28 2-1 2-3 2-5 2-9 2-11 2-16 2-18 2-22 2-24 2-26 3-2 3-4 3-8 3-10 3-12 3-16 3-18 3-22 3-24 3-26 4-6 4-8 4-12 4-14 4-16 4-20 4-22 4-26 4-28 4-30 5-5 5-7 5-11 5-14 5-18 5-20 5-24 5-26 5-28 6-2 6-4 6-8 6-10 ";
 const document_title = ["Morning", "Advisory", "Transition", "1st", "Transition", "2nd", "Transition", "3rd", "Transition", "4th", "Transition", "5th", "Transition", "6th", "Transition", "7th", "Out"];
+const table_header = ["Advisory", "1<sup>st</sup> Period", "2<sup>nd</sup> Period", "3<sup>rd</sup> Period","4<sup>th</sup> Period", "5<sup>th</sup> Period","6<sup>th</sup> Period", "7<sup>th</sup> Period"];
 const next_event = ["Advisory Starts", "Advisory Ends", "First Period Starts", "First Period Ends","Second Period Starts", "Second Period Ends","Third Period Starts", "Third Period Ends","Fourth Period Starts", "Fourth Period Ends","Fifth Period Starts", "Fifth Period Ends","Sixth Period Starts", "Sixth Period Ends","Seventh Period Starts", "Seventh Period Ends"];
-const schedules = [
+let notifications = null;
+let schedules = [
     {
         id: "scheduleA",
-        text: "Monday/Tuesday Regular Schedule",
+        text: "Regular Schedule",
         times: [8*60, 8*60+32, 8*60+37, 9*60+28, 9*60+33, 10*60+24, 10*60+29, 11*60+20, 11*60+25, 12*60+16, 12*60+21, 13*60+12, 13*60+17, 14*60+8, 14*60+13, 15*60+4],
         title: document_title,
-        next_event: next_event
-    }, 
-    {
-        id: "scheduleB",
-        text: "Wednesday Delayed Opening Schedule",
-        times: [9*60, 9*60+28, 9*60+33, 10*60+16, 10*60+21, 11*60+4, 11*60+9, 11*60+52, 11*60+57, 12*60+40, 12*60+45, 13*60+28, 13*60+33, 14*60+16, 14*60+21, 15*60+04],
-        title: document_title,
-        next_event: next_event        
+        next_event: next_event,
+        table_header: table_header
     }, 
     {
         id: "scheduleC",
-        text: "Thursday/Friday 1 Hour Advisory",
+        text: "1 Hour Advisory",
         times: [8*60, 9*60, 9*60+5, 9*60+52, 9*60+57, 10*60+44, 10*60+49, 11*60+36, 11*60+41, 12*60+28, 12*60+33, 13*60+20, 13*60+25, 14*60+12, 14*60+17, 15*60+4],
         title: document_title,
-        next_event: next_event
+        next_event: next_event,
+        table_header: table_header
     }, 
     {
         id: "scheduleD",
-        text: "Delayed Opening",
+        text: "Weather Delay",
         times: [10*60, 10*60+31, 10*60+36, 11*60+10, 11*60+15, 11*60+49, 11*60+54, 12*60+28, 12*60+33, 13*60+07, 13*60+12, 13*60+46, 13*60+51, 14*60+25, 14*60+30, 15*60+4],
         title: document_title,
-        next_event: next_event
+        next_event: next_event,
+        table_header: table_header
     }, 
     {
         id: "scheduleE",
-        text: "Half Day",
+        text: "Early Dismissal",
         times: [8*60, 8*60+34, 8*60+39, 9*60+4, 9*60+09, 9*60+34, 9*60+39, 10*60+4, 10*60+9, 10*60+34, 10*60+39, 11*60+4, 11*60+9, 11*60+34, 11*60+39, 12*60+4],
         title: document_title,
-        next_event: next_event
+        next_event: next_event,
+        table_header: table_header
     }
 ];
 var bell_on = false;
@@ -53,27 +52,38 @@ function get_td_id(i, j) {
 }
 
 function buildHTML() {
+    let max_cols = schedules[current_schedule_index].table_header.length;
+    for (let i = 0; i < schedules.length; i ++) {
+        if (max_cols < schedules[i].times.length/2) max_cols = schedules[i].times.length/2;
+    }
     let table = "<table>";
     table += "<thead>";
     table += "<tr>";
     table += "<th></th>";
-    table += "<th>Advisory</th>";
-    table += "<th>1<sup>st</sup> Period</th>";
-    table += "<th>2<sup>nd</sup> Period</th>";
-    table += "<th>3<sup>rd</sup> Period</th>";
-    table += "<th>4<sup>th</sup> Period</th>";
-    table += "<th>5<sup>th</sup> Period</th>";
-    table += "<th>6<sup>th</sup> Period</th>";
-    table += "<th>7<sup>th</sup> Period</th>";
+    for ( let i = 0; i < schedules[current_schedule_index].table_header.length; i ++) {
+        if (i ==  schedules[current_schedule_index].table_header.length - 1) {
+            let grow = (max_cols - schedules[current_schedule_index].table_header.length) + 1;
+            table += "<th colspan="+ grow +">" + schedules[current_schedule_index].table_header[i] + "</th>";
+        }
+        else table += "<th>" + schedules[current_schedule_index].table_header[i] + "</th>";
+    }
     table += "</tr>";
     table += "</thead>";
     table += "<tbody>";
     for (let i = 0; i < schedules.length; i ++) {
         table += "<tr id='" + schedules[i].id + "' onclick = 'set_schedule(" + i + ");'>";
         table += "<th>" + schedules[i].text + "</th>";
-        for (j = 0; j < schedules[i].times.length/2; j ++) {
-            table += "<td id = '" + get_td_id(i, j) + "'>" + time_from_minutes(schedules[i].times[j*2]) + "-" + time_from_minutes(schedules[i].times[j*2+1]) + "</td>";
+        if (schedules[i].times.length == 0) {
+            table += "<td colspan="+ max_cols +">nothing is scheduled</td>";
         }
+        for (j = 0; j < schedules[i].times.length/2; j ++) {
+            if (j ==  schedules[i].times.length/2 - 1) {
+                let grow = (max_cols - schedules[i].table_header.length) + 1;
+                table += "<td id = '" + get_td_id(i, j) + "' colspan="+ grow +">" + time_from_minutes(schedules[i].times[j*2]) + "-" + time_from_minutes(schedules[i].times[j*2+1]) + "</td>";
+            }
+            else table += "<td id = '" + get_td_id(i, j) + "'>" + time_from_minutes(schedules[i].times[j*2]) + "-" + time_from_minutes(schedules[i].times[j*2+1]) + "</td>";
+        }
+
         table += "</tr>";
     }
     table += "</tbody>";
@@ -123,11 +133,11 @@ function build_birthdays() {
 }
 
 function set_schedule(index) {
-
     for (i = 0; i < schedules[current_schedule_index].times.length; i++)
         document.getElementById(get_my_td_id(Math.floor(i/2))).className = "";            
 
     current_schedule_index = index;
+    buildHTML();
     for (i = 0; i < schedules.length; i ++) {
         if (i == current_schedule_index)
             document.getElementById(schedules[i].id).className = "";
@@ -186,7 +196,7 @@ function settime() {
             if (seconds_left < 10) seconds_left = "0" + seconds_left;
             if (minutes_left < 10) minutes_left = "0" + minutes_left;
 
-            document.getElementById("message").innerHTML = next_event[i] + " in " + hours_left + ":" + minutes_left + ":" + seconds_left;
+            document.getElementById("message").innerHTML = schedules[current_schedule_index].next_event[i] + " in " + hours_left + ":" + minutes_left + ":" + seconds_left;
 
             document.getElementById(get_my_td_id(Math.floor(i/2))).className = "active";
 
@@ -215,7 +225,6 @@ function settime() {
         }
     }
 }
-
 
 function playAudio() { 
     var bell; 
@@ -261,19 +270,87 @@ function set_date() {
     document.getElementById("date").innerHTML = day[d.getDay()] +""+ (d.getMonth() + 1) + "/" + d.getDate() + "<br><i>" + aDayString + "</i>";
 }
 
-function main() {
-     var d = new Date();
-     buildHTML();
+async function getCalendarData () {
+    try {
+        const calendarId = 'c_jqklkgt4mu0sn4jn1v4mvkmfts@group.calendar.google.com'
+        const myKey = 'AIzaSyAPTC1zaYu2RAtwn381b0RPBzkaka4jfOo'
+        let now = new Date();
+        now.setHours(0,0,0,0);
+        let begin_time = now.toISOString()
+        now.setHours(23,59,0,0);
+        let end_time = now.toISOString()
+        let url = 'https://www.googleapis.com/calendar/v3/calendars/' + calendarId+ '/events?key=' + myKey + '&singleEvents=True&maxResults=30&orderBy=startTime&singleEvents=true&timeMin=' + begin_time + '&timeMax=' + end_time;
+        let apiCall = await fetch( url );
+        let apiResponse = await apiCall.json();
+        if (apiResponse.items.length > 0) {
+            let online_schedule = [];
+            let online_table_header = [];
+            let online_next_event = [];
+            let online_document_title = ["Morning"];
+            for (let i = 0; i < apiResponse.items.length; i ++) {
+                if (apiResponse.items[i].start != null) {
+                    let d = new Date(apiResponse.items[i].start.dateTime);
+                    if (d != 'Invalid Date') {
+                        let minutes = d.getHours()*60+d.getMinutes();
+                        online_schedule.push(minutes);
+                        d = new Date(apiResponse.items[i].end.dateTime);
+                        minutes = d.getHours()*60+d.getMinutes();
+                        online_schedule.push(minutes);
+                        online_table_header.push(apiResponse.items[i].summary);
+                        online_next_event.push(apiResponse.items[i].summary + " Starts");
+                        online_next_event.push(apiResponse.items[i].summary + " Ends");
+                        online_document_title.push(apiResponse.items[i].summary);
+                        online_document_title.push("Transition");
+                    }
+                }
+            }
+            buildToday( {
+                id: "schedule",
+                text: "Palumbo Google Calendar",
+                times: online_schedule,
+                title: online_document_title,
+                next_event: online_next_event,
+                table_header: online_table_header
+            });
+        }
+        else {
+            buildToday({
+                id: "schedule",
+                text: "Palumbo Google Calendar",
+                times: [],
+                title: ['notining is scheduled'],
+                next_event: [],
+                table_header: ["nothing is scheduled"]
+            });
+        }
+    } catch (error) {
+        buildToday(false);
+    }
+}
 
-    if (d.getDay() == 3) //wednesday
-        set_schedule(1);
-    else if (d.getDay() == 5) //friday
-        set_schedule(2);
-    else if (d.getDay() == 4) //thursday
-        set_schedule(2);
-    else
+function main() {
+    getCalendarData();    
+}
+
+function buildToday(onlineExisits) {
+    buildHTML();
+    if (onlineExisits != false) {
+        schedules.unshift(
+            onlineExisits
+        );
         set_schedule(0);
-            
+    }
+    else {
+        var d = new Date();
+        if (d.getDay() == 3) //wednesday
+            set_schedule(1);
+        else if (d.getDay() == 5) //friday
+            set_schedule(2);
+        else if (d.getDay() == 4) //thursday
+            set_schedule(2);
+        else
+            set_schedule(0);
+    }            
     settime();
 	build_birthdays();
     var interval = setInterval(settime, 1000);
@@ -287,3 +364,32 @@ function time_from_minutes(time) {
     else if (minutes < 10) minutes = "0" + minutes;
     return hours+":"+minutes;
 }
+
+let noteallowed = false;
+
+function push_notifications() {
+    if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert("This browser does not support desktop notification");
+      console.log("1");
+    } else if (Notification.permission === "granted") {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification("Hi there!");
+      // …
+      console.log("2");
+    } else if (Notification.permission !== "denied") {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          const notification = new Notification("Hi there!");
+          console.log("3");
+          // …
+        }
+      });
+    }
+  
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+  }
